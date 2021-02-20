ae763a147dc86e64c7f3f431d9bbb549d629bf18
vmagnin <vincent.magnin@libertysurf.fr>
2021-02-20 21:14:54 +0100

    Optimized ADSR_envelope() function

--- src/envelopes.f90
+++ src/envelopes.f90
@@ -29,26 +29,34 @@ contains
         real(kind=dp), intent(in) :: t, t1, t2
         integer :: i, i1, i2, i3, i4, i5
 
-        ! Different parts:
+        i = nint(t * RATE)
+
+        ! First part (Attack):
         i1 = nint(t1 * RATE)
         i2 = nint((t1 + (t2-t1) * attack / 100.0_dp) * RATE)
-        i3 = nint((t1 + (t2-t1) * (attack+decay) / 100.0_dp) * RATE)
-        i4 = nint((t2 - (t2-t1) * release / 100.0_dp) * RATE)
-        i5 = nint(t2 * RATE)
-
-        i = nint(t * RATE)
 
         if ((i >= i1) .and. (i < i2)) then
             ADSR_enveloppe = (i-i1) / real(i2-i1, dp)
-        else if ((i >= i2) .and. (i < i3)) then
-            ADSR_enveloppe = (100.0_dp - (i-i2)/real(i3-i2, dp) * (100.0_dp-sustain)) / 100.0_dp
-        else if ((i >= i3) .and. (i < i4)) then
-            ADSR_enveloppe = (sustain / 100.0_dp)
-        else if ((i >= i4) .and. (i < i5)) then
-            ADSR_enveloppe = (sustain - (i-i4)/real(i5-i4, dp) * sustain) / 100.0_dp
         else
-            print *, "ERROR ADSR_envelope: t outside [t1, t2] !"
-            ADSR_enveloppe = 1.0_dp
+            i3 = nint((t1 + (t2-t1) * (attack+decay) / 100.0_dp) * RATE)
+            if ((i >= i2) .and. (i < i3)) then
+                ADSR_enveloppe = (100.0_dp - (i-i2)/real(i3-i2, dp) * &
+                               & (100.0_dp-sustain)) / 100.0_dp
+            else
+                i4 = nint((t2 - (t2-t1) * release / 100.0_dp) * RATE)
+                if ((i >= i3) .and. (i < i4)) then
+                    ADSR_enveloppe = (sustain / 100.0_dp)
+                else
+                    i5 = nint(t2 * RATE)
+                    if ((i >= i4) .and. (i < i5)) then
+                        ADSR_enveloppe = (sustain - (i-i4)/real(i5-i4, dp) * &
+                                       & sustain) / 100.0_dp
+                    else
+                        print *, "ERROR ADSR_envelope: t outside [t1, t2] !"
+                        ADSR_enveloppe = 1.0_dp
+                    end if
+                end if
+            end if
         end if
     end function
 
