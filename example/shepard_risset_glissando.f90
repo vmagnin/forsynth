d616ee4c94cd49c7be330f6d331499690efc8c26
vmagnin <vincent.magnin@libertysurf.fr>
2024-06-01 15:44:46 +0200

    example/shepard_risset_glissando.f90 improved but not yet perfect...

--- example/shepard_risset_glissando.f90
+++ example/shepard_risset_glissando.f90
@@ -5,6 +5,11 @@
 
 ! A Shepard-Risset glissando, giving the illusion of an ever increasing pitch.
 ! It is the continuous version of the Shepard scale.
+! It is not perfect, as we can hear that globally the whole is getting slowly
+! higher. It is also visible when zooming in the waveform woth audacity.
+! Some kind of beating might occur due to the fact that in the sin(),
+! both omega and t are varying at each step. Or a slight desynchronization
+! of the octaves could occur while multiplying f(j) ?
 ! https://en.wikipedia.org/wiki/Shepard_tone
 program shepard_risset_glissando
     use forsynth, only: wp, dt, RATE, PI
@@ -22,24 +27,24 @@ program shepard_risset_glissando
     !--------------------------
     ! Glissando parameters:
     !--------------------------
-    ! Duration of each component in seconds:
-    real(wp), parameter :: d = 16._wp
-    ! 20-10000 kHz: 9 octaves
-    ! It seems that going higher could introduce artefacts, probably because
-    ! we approach the sampling rate (44100 Hz).
-    integer, parameter  :: cmax = 9
-    ! Bandwidth:
-    real(wp), parameter :: fmin = 20._wp         ! Hz
-    real(wp), parameter :: fmax = 10240._wp      ! Hz
+    ! Bandwidth 20-20480 Hz: 10 octaves
+    integer, parameter  :: cmax = 10
+    real(wp), parameter :: fmin = 20._wp
+    real(wp), parameter :: fmax = fmin * 2**cmax
     ! Frequencies of each component:
     real(wp) :: f(cmax)
-    ! Gaussian window, central frequency in log scale:
-    real(wp), parameter :: muf  = (log10(fmin) + log10(fmax)) / 2
+    ! Gaussian window, central frequency in log scale, with a shift:
+    real(wp), parameter :: muf  = ((log10(fmin) + log10(fmax)) / 2) - 0.3
     ! Standard deviation (very important for a good result):
     real(wp), parameter :: sigma = 0.25_wp
     ! Total duration of the WAV:
     real(wp), parameter :: length = 120._wp
+    ! Setting the increase rate:
+    real(wp), parameter :: d = 16._wp
     real(wp), parameter :: increase = 2**(+1/(d*RATE))
+
+    ! Useful for debugging and setting the envelope parameters:
+    !call write_amplitude_envelope()
 
     ! Initializing the components, separated by octaves:
     do j = 1, cmax
@@ -61,7 +66,7 @@ program shepard_risset_glissando
         do j = 1, cmax
             omega = 2*PI*f(j)
             ! Amplitude of the signal (gaussian distribution):
-            Amp = 1/(sqrt(2*PI)*sigma) * exp(-(log10(f(j)) - muf)**2 / (2 * sigma**2))
+            Amp = amplitude(f(j))
 
             tape%left(1, i)  = tape%left(1, i) + Amp * sin(omega*t)
         end do
@@ -73,10 +78,11 @@ program shepard_risset_glissando
             ! Increasing pitch:
             f(j) = f(j) * increase
             ! Each component must stay between fmin and fmax:
-            if (f(j) > fmax) then
+            if (f(j) >= fmax) then
                 f(j) = fmin
                 print *, j, "f(j) > fmax"
-            else if (f(j) < fmin) then
+            else if (f(j) <= fmin) then
+                ! Would be useful for a decreasing glissando:
                 f(j) = fmax
             end if
         end do
@@ -93,4 +99,62 @@ program shepard_risset_glissando
     call demo%close_WAV_file()
 
     print *,"You can now play the file ", demo%get_name()
+
+contains
+
+    ! Returns an amplitude rising from 0 to 1, from f1 to f2. And 0 outside.
+    real(wp) function linear1(freq, f1, f2)
+        real(wp), intent(in) :: freq, f1, f2
+
+        if ((f1 <= freq).and.(freq <= f2)) then
+            linear1 = (freq - f1) / (f2 - f1)
+        else
+            linear1 = 0
+        end if
+    end function
+
+    ! Returns an amplitude falling from 1 to 0, from f1 to f2. And 0 outside.
+    real(wp) function linear2(freq, f1, f2)
+        real(wp), intent(in) :: freq, f1, f2
+
+        if ((f1 <= freq).and.(freq <= f2)) then
+            linear2 = (freq - f2) / (f1 - f2)
+        else
+            linear2 = 0
+        end if
+    end function
+
+    ! Envelope of the glissando. A gaussian, plus linear sections at
+    ! the extremities, to reach the 0 level.
+    real(wp) function amplitude(freq)
+        real(wp), intent(in) :: freq
+        real(wp) :: Amp
+
+        if (freq <= 2*fmin) then
+            Amp = linear1(freq, fmin, 2*fmin)
+        else if (freq >= fmax/2) then
+            Amp = linear2(freq, fmax/2, fmax)
+        else
+            Amp = 1
+        end if
+
+        amplitude = Amp * (1/(sqrt(2*PI)*sigma)) * exp(-(log10(freq) - muf)**2 / (2 * sigma**2))
+    end function
+
+    ! Useful for debugging and setting the envelope parameters:
+    subroutine write_amplitude_envelope()
+        real(wp) ::freq
+        integer  :: u
+        integer, parameter :: points = 500
+
+        print *, muf, " muf = ", 10**muf
+
+        open(newunit=u, file="glissando_envelope.txt", status='replace', action='write')
+        do i = 1, points
+            freq = fmin + i*(fmax-fmin) / points
+            write(u, *) freq, amplitude(freq)
+        end do
+        close(u)
+    end subroutine
+
 end program shepard_risset_glissando
