d660c9b097336117f76cbccffd1b2fc517c7847d
vmagnin <vincent.magnin@libertysurf.fr>
2025-03-02 14:44:11 +0100

    test/main.f90: added a assert_reals_equal() subroutine

--- test/main.f90
+++ test/main.f90
@@ -1,9 +1,10 @@
 ! Forsynth: a multitracks stereo sound synthesis project
 ! License GPL-3.0-or-later
 ! Vincent Magnin
-! Last modification: 2025-02-02
+! Last modification: 2025-03-02
 
 program main
+    use, intrinsic :: ieee_arithmetic, only: ieee_is_nan
     use forsynth, only: wp, test_the_machine
     use signals, only: weierstrass
     use morse_code
@@ -24,4 +25,27 @@ program main
     if (string_to_morse("DISCOVERED BY MADAME CURIE") /= &
         & "-.. .. ... -.-. --- ...- . .-. . -..  -... -.--  -- .- -.. .- -- .  -.-. ..- .-. .. .") &
         & error stop "ERROR string_to_morse() [2]"
+
+    call assert_reals_equal(10._wp ** (-20._wp / 20._wp), 0.1_wp, 1e-6_wp)
+
+contains
+
+    ! A routine testing if a=b within the given tolerance.
+    subroutine assert_reals_equal(a, b, tolerance)
+        real(wp), intent(in)        :: a, b, tolerance
+        real(wp)                    :: r
+        character(len=*), parameter :: FORMATER = '(a,es22.15,a,es22.15,a,es8.1,a)'
+
+        if (b /= 0.0_wp) then
+            r = abs((a - b)/b)
+        else
+            r = abs(a - b)
+        end if
+
+        ! Note that the -Ofast compiler option is disabling ieee_is_nan()
+        if (ieee_is_nan(a) .or. (r > tolerance)) then
+            print FORMATER, "*** bug *** ", b, " /= ", a, " ; r = ", r
+        end if
+    end subroutine assert_reals_equal
+
 end program main
