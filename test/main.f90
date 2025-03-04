829cea32e8ea1838c8bb3ee8bda16bb1501dc06f
vmagnin <vincent.magnin@libertysurf.fr>
2025-03-04 19:28:14 +0100

    test/main.f90: modified tolerance (now works with REAL32)

--- test/main.f90
+++ test/main.f90
@@ -15,7 +15,7 @@ program main
     implicit none
 
     ! 2.2204460492503131E-016 with real64:
-    real(wp), parameter :: tol = epsilon(1._wp)
+    real(wp), parameter :: tol = 10*epsilon(1._wp)
 
     ! Test the available KIND:
     call test_the_machine()
@@ -32,36 +32,36 @@ program main
         & error stop "ERROR string_to_morse() [2]"
 
     !************************************************
-    call assert_reals_equal(dB_to_linear(  0._wp), 1.0_wp,     tolerance=3*tol)
+    call assert_reals_equal(dB_to_linear(  0._wp), 1.0_wp,     tolerance=tol)
     call assert_reals_equal(dB_to_linear(-06._wp), 0.50118_wp, tolerance=1e-4_wp)
-    call assert_reals_equal(dB_to_linear(-20._wp), 0.1_wp,     tolerance=3*tol)
-    call assert_reals_equal(dB_to_linear(+20._wp), 10._wp,     tolerance=3*tol)
+    call assert_reals_equal(dB_to_linear(-20._wp), 0.1_wp,     tolerance=tol)
+    call assert_reals_equal(dB_to_linear(+20._wp), 10._wp,     tolerance=tol)
 
     !************************************************
     call assert_reals_equal(fit_exp(-1._wp, x1=0._wp, y1=1._wp, x2=1._wp, y2=exp(1._wp)), &
-                          & exp(-1._wp), tolerance=3*tol)
+                          & exp(-1._wp), tolerance=tol)
     call assert_reals_equal(fit_exp(0._wp, x1=-1._wp, y1=exp(-1._wp), x2=+1._wp, y2=exp(+1._wp)), &
-                          & 1._wp, tolerance=3*tol)
+                          & 1._wp, tolerance=tol)
 
     !************************************************
-    call assert_reals_equal(fr("A4"),  440.0_wp, tolerance=3*tol)
-    call assert_reals_equal(fr("A3"),  220.0_wp, tolerance=3*tol)
-    call assert_reals_equal(fr("A5"),  880.0_wp, tolerance=3*tol)
-    call assert_reals_equal(fr("G4"),  440.0_wp*2**(-2/12._wp), tolerance=3*tol)
-    call assert_reals_equal(fr("F#4"), 440.0_wp*2**(-3/12._wp), tolerance=3*tol)
-    call assert_reals_equal(fr("Fb4"), 440.0_wp*2**(-5/12._wp), tolerance=3*tol)
+    call assert_reals_equal(fr("A4"),  440.0_wp, tolerance=tol)
+    call assert_reals_equal(fr("A3"),  220.0_wp, tolerance=tol)
+    call assert_reals_equal(fr("A5"),  880.0_wp, tolerance=tol)
+    call assert_reals_equal(fr("G4"),  440.0_wp*2**(-2/12._wp), tolerance=tol)
+    call assert_reals_equal(fr("F#4"), 440.0_wp*2**(-3/12._wp), tolerance=tol)
+    call assert_reals_equal(fr("Fb4"), 440.0_wp*2**(-5/12._wp), tolerance=tol)
 
     !************************************************
     block
         type(ADSR_envelope) :: env
         call env%new(A=30._wp, D=20._wp, S=80._wp, R=30._wp)
-        call assert_reals_equal(env%get_level(t=0._wp,  t1=0._wp, t2=100._wp),  0._wp,  tolerance=3*tol)
-        call assert_reals_equal(env%get_level(t=100._wp,t1=0._wp, t2=100._wp),  0._wp,  tolerance=3*tol)
-        call assert_reals_equal(env%get_level(t=30._wp, t1=0._wp, t2=100._wp),  1._wp,  tolerance=3*tol)
-        call assert_reals_equal(env%get_level(t=15._wp, t1=0._wp, t2=100._wp),  0.5_wp, tolerance=3*tol)
-        call assert_reals_equal(env%get_level(t=40._wp, t1=0._wp, t2=100._wp),  0.9_wp, tolerance=3*tol)
-        call assert_reals_equal(env%get_level(t=60._wp, t1=0._wp, t2=100._wp),  0.8_wp, tolerance=3*tol)
-        call assert_reals_equal(env%get_level(t=85._wp, t1=0._wp, t2=100._wp),  0.4_wp, tolerance=3*tol)
+        call assert_reals_equal(env%get_level(t=0._wp,  t1=0._wp, t2=100._wp),  0._wp,  tolerance=tol)
+        call assert_reals_equal(env%get_level(t=100._wp,t1=0._wp, t2=100._wp),  0._wp,  tolerance=tol)
+        call assert_reals_equal(env%get_level(t=30._wp, t1=0._wp, t2=100._wp),  1._wp,  tolerance=tol)
+        call assert_reals_equal(env%get_level(t=15._wp, t1=0._wp, t2=100._wp),  0.5_wp, tolerance=tol)
+        call assert_reals_equal(env%get_level(t=40._wp, t1=0._wp, t2=100._wp),  0.9_wp, tolerance=tol)
+        call assert_reals_equal(env%get_level(t=60._wp, t1=0._wp, t2=100._wp),  0.8_wp, tolerance=tol)
+        call assert_reals_equal(env%get_level(t=85._wp, t1=0._wp, t2=100._wp),  0.4_wp, tolerance=tol)
     end block
 
 contains
