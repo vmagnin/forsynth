dd061dbe46ba138c7f383319b8cf1f653e059032
vmagnin <vincent.magnin@libertysurf.fr>
2025-03-04 20:58:36 +0100

    example/misc_sounds.f90: added an auxilliary variable

--- example/misc_sounds.f90
+++ example/misc_sounds.f90
@@ -44,6 +44,7 @@ contains
         real(wp) :: omega
         ! Time in seconds:
         real(wp) :: t
+        real(wp) :: signal
         integer  :: i, j
 
         omega = 2 * PI * f
@@ -51,71 +52,72 @@ contains
         do i = nint(t1*RATE), nint(t2*RATE)-1
             select case (choice)
                 case (0) ! Pure sinus
-                    tape%left(track, i) = + Amp * sin(omega*t)
+                    signal = + Amp * sin(omega*t)
                 case (1) ! Science fiction signal...
-                    tape%left(track, i) = + Amp * sin(omega*t + cos(75*t)/(1+log(t/100+0.01_wp)))
+                    signal = + Amp * sin(omega*t + cos(75*t)/(1+log(t/100+0.01_wp)))
                 case (2) ! Hummmmmmmmmm...
-                    tape%left(track, i) = + Amp * sin(omega*t + sin(omega*0.2_wp*t))
+                    signal = + Amp * sin(omega*t + sin(omega*0.2_wp*t))
                 case (3) ! UFO or siren?
-                    tape%left(track, i) = + Amp * sin(omega*t + cos(25*t))
+                    signal = + Amp * sin(omega*t + cos(25*t))
                 case (4) ! Noisy science fiction
-                    tape%left(track, i) = + Amp * sin(omega*(1._wp + 0.2_wp*sin(t*50)) * t)
+                    signal = + Amp * sin(omega*(1._wp + 0.2_wp*sin(t*50)) * t)
                 case (5) ! Whistling, slower and slower...
-                    tape%left(track, i) = + Amp * sin(omega*(1._wp + 0.2_wp*sin(sqrt(t)*50)) * t)
+                    signal = + Amp * sin(omega*(1._wp + 0.2_wp*sin(sqrt(t)*50)) * t)
                 case (6) ! Similar
-                    tape%left(track, i) = + Amp * sin(omega*(1._wp + 0.2_wp*sin(log(t)*50)) * t)
+                    signal = + Amp * sin(omega*(1._wp + 0.2_wp*sin(log(t)*50)) * t)
                 case (7) ! Dudududududu...
-                    tape%left(track, i) = + Amp * sin(t*30) * sin(omega * t)
+                    signal = + Amp * sin(t*30) * sin(omega * t)
                 case (8) ! Duuuuuuuuuuuuuuuuuuu
-                    tape%left(track, i) = + Amp * sin(omega*t + 0.5_wp+0.5_wp*sin(t*500))
+                    signal = + Amp * sin(omega*t + 0.5_wp+0.5_wp*sin(t*500))
                 case (9) ! A higher duuuuuuuuuuuuuuuuuuu
-                    tape%left(track, i) = + Amp/2 * sin(omega*t) + Amp/2 * sin(omega*1.1892_wp*t)
+                    signal = + Amp/2 * sin(omega*t) + Amp/2 * sin(omega*1.1892_wp*t)
                 case (10) ! Higher and higher...
-                    tape%left(track, i) = + Amp * sin((omega*(1+t/10)) * t)
+                    signal = + Amp * sin((omega*(1+t/10)) * t)
                 case (11) ! Dampening slowly
                     do j=1, 7
-                        tape%left(track, i) = + Amp/(j*(1+t**j))*sin(j*omega*t)
+                        signal = + Amp/(j*(1+t**j))*sin(j*omega*t)
                     end do
                 case (12) ! Vibrato
-                    tape%left(track, i) = + Amp * sin(omega*t + 1._wp + 0.5_wp*sin(2*PI*4*t))
+                    signal = + Amp * sin(omega*t + 1._wp + 0.5_wp*sin(2*PI*4*t))
                 case (13) ! A mix
-                    tape%left(track, i) = + Amp * sin(t) * sin(omega*t) &
-                                   & + Amp * cos(2.5_wp*t) * sin(1.5_wp*omega*t) + Amp * sin(3*t) * sin(2*omega*t)
+                    signal = + Amp * sin(t) * sin(omega*t) &
+                           & + Amp * cos(2.5_wp*t) * sin(1.5_wp*omega*t) + Amp * sin(3*t) * sin(2*omega*t)
                 case (14) ! Tremolo
-                    tape%left(track, i) = + Amp * sin(omega*t) + Amp * sin(1.001_wp*omega*t) &
-                                   & + Amp * sin(0.999_wp*omega*t)
+                    signal = + Amp * sin(omega*t) + Amp * sin(1.001_wp*omega*t) &
+                           & + Amp * sin(0.999_wp*omega*t)
                 case (15) ! Poke... (a short percussion based on the Sinc function)
                     if (omega*t /= 0._wp) then
-                        tape%left(track, i) = + Amp * sin(omega*t) / (omega*t)
+                        signal = + Amp * sin(omega*t) / (omega*t)
                     else
-                        tape%left(track, i) = + Amp
+                        signal = + Amp
                     end if
                 case (16) ! Science fiction, becoming higher and noisy
-                    tape%left(track, i) = + Amp * sin(omega*(1._wp + 0.001_wp*sin(t*500)) * t)
+                    signal = + Amp * sin(omega*(1._wp + 0.001_wp*sin(t*500)) * t)
                 case (17) ! Dissonant
-                    tape%left(track, i) = + Amp*sin(omega*t + 4*sin(omega/10 * t))
+                    signal = + Amp*sin(omega*t + 4*sin(omega/10 * t))
                 case (18) ! Dampening
-                    tape%left(track, i) = + Amp*(1/(1+t**3))*sin(omega*t + 2*sin(omega*t+3*sin(omega*t)))
+                    signal = + Amp*(1/(1+t**3))*sin(omega*t + 2*sin(omega*t+3*sin(omega*t)))
                 case (19) ! Science-fiction, becoming higher and noisy
-                    tape%left(track, i) = + Amp * &
-                                   & sin(omega*(1._wp + 0.001_wp*sin(t*500*(1._wp + 0.0002_wp*cos(t*50)))) * t)
+                    signal = + Amp &
+                           & * sin(omega*(1._wp + 0.001_wp*sin(t*500*(1._wp + 0.0002_wp*cos(t*50)))) * t)
                 case (20) ! Another dampening sound
-                    tape%left(track, i) = + Amp*(exp(-t)*sin(omega*t) + 0.5_wp*exp(-t**2)*sin(2*omega*t) &
-                                   & + 0.25_wp*exp(-t**3)*sin(3*omega*t) + 0.12_wp*exp(-t**4)*sin(4*omega*t))
+                    signal = + Amp*(exp(-t)*sin(omega*t) + 0.5_wp*exp(-t**2)*sin(2*omega*t) &
+                           & + 0.25_wp*exp(-t**3)*sin(3*omega*t) + 0.12_wp*exp(-t**4)*sin(4*omega*t))
                 case (21) ! Dampening slowly
                     do j=1, 14
-                        tape%left(track, i) = + Amp/(j**2 * (1+t**j))*sin(j*omega*t)
+                        signal = + Amp/(j**2 * (1+t**j))*sin(j*omega*t)
                     end do
                 case (22) ! Clarinet
                     do j=1, 11, +2
-                        tape%left(track, i) = + Amp/j**0.7_wp * sin(j*omega*t)
+                        signal = + Amp/j**0.7_wp * sin(j*omega*t)
                     end do
                 case (23) ! Bessel function of the first kind J1: pong...
-                    tape%left(track, i) = + Amp * bessel_jn(1, omega*t)
+                    signal = + Amp * bessel_jn(1, omega*t)
                 case (24) ! Bessel functions of the first kind: ping (far shorter)
-                    tape%left(track, i) = + Amp * bessel_jn(1, omega*t) * bessel_jn(2, omega*t)
+                    signal = + Amp * bessel_jn(1, omega*t) * bessel_jn(2, omega*t)
             end select
-            tape%right(track, i) = tape%left(track, i)
+            tape%left (track, i) = signal
+            tape%right(track, i) = signal
             t = t + dt
         end do
     end subroutine add_misc_signal
